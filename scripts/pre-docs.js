c66bc802c098deb529bab6f6f665177ff90b18e2
Yousif Al-Raheem <yousifalraheem@gmail.com>
2024-11-02 10:02:58 +0100

    build: add support for rollup version 4
    #67

--- scripts/pre-docs.js
+++ scripts/pre-docs.js
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
 const { readFileSync, writeFileSync } = require("fs");
 const { removeSync, copySync } = require("fs-extra");
 const { resolve } = require("path");
